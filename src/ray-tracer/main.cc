df1ffc0abcb2f438e64f16a61b179426aaf6803e
Nicholas Tang <ntang11@ucsc.edu>
2025-11-18 12:09:45 -0800

    minor changes

--- src/ray-tracer/main.cc
+++ src/ray-tracer/main.cc
@@ -64,7 +64,7 @@ int main() {
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 360;
+    cam.image_width       = 256;
     cam.samples_per_pixel = 10;
     cam.max_depth         = 20;
 
--- src/ray-tracer/vec3.h
+++ src/ray-tracer/vec3.h
@@ -12,53 +12,55 @@
 //==============================================================================================
 
 class vec3 {
-  public:
-    double e[3];
+    public:
+        double e[3];
 
-    vec3() : e{0,0,0} {}
-    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+        vec3() : e{0,0,0} {}
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
 
-    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i]; }
-    double& operator[](int i) { return e[i]; }
+        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
 
-    vec3& operator+=(const vec3& v) {
-        return *this;
-    }
+        vec3& operator+=(const vec3& v) {
+            //  TODO: complete
+            return *this;
+        }
 
-    vec3& operator*=(double t) {
-        return *this;
-    }
+        vec3& operator*=(double t) {
+            //  TODO: complete
+            return *this;
+        }
 
-    vec3& operator/=(double t) {
-        return *this *= 1/t;
-    }
+        vec3& operator/=(double t) {
+            return *this *= 1/t;
+        }
 
-    double length() const {
-        return std::sqrt(length_squared());
-    }
+        double length() const {
+            return std::sqrt(length_squared());
+        }
 
-    double length_squared() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-    }
+        double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
 
-    bool near_zero() const {
-        // Return true if the vector is close to zero in all dimensions.
-        auto s = 1e-8;
-        return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
-    }
+        bool near_zero() const {
+            // Return true if the vector is close to zero in all dimensions.
+            auto s = 1e-8;
+            return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s);
+        }
 
-    static vec3 random() {
-        return vec3(random_double(), random_double(), random_double());
-    }
+        static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
 
-    static vec3 random(double min, double max) {
-        return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
-    }
+        static vec3 random(double min, double max) {
+            return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+        }
 };
 
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
@@ -92,13 +94,14 @@ inline vec3 operator/(const vec3& v, double t) {
 }
 
 inline double dot(const vec3& u, const vec3& v) {
+    //  TODO: complete
     return 0.0;
 }
 
 inline vec3 cross(const vec3& u, const vec3& v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+            u.e[2] * v.e[0] - u.e[0] * v.e[2],
+            u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(const vec3& v) {
