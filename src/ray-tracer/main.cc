41d98eba4369f2ecbd49720e6c73530ac5273491
Nicholas Tang <ntang11@ucsc.edu>
2025-11-17 20:57:23 -0800

    remove some vec3 functions

--- src/ray-tracer/main.cc
+++ src/ray-tracer/main.cc
@@ -64,7 +64,7 @@ int main() {
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 480;
+    cam.image_width       = 360;
     cam.samples_per_pixel = 10;
     cam.max_depth         = 20;
 
--- src/ray-tracer/vec3.h
+++ src/ray-tracer/vec3.h
@@ -27,16 +27,10 @@ class vec3 {
     double& operator[](int i) { return e[i]; }
 
     vec3& operator+=(const vec3& v) {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        e[2] += v.e[2];
         return *this;
     }
 
     vec3& operator*=(double t) {
-        e[0] *= t;
-        e[1] *= t;
-        e[2] *= t;
         return *this;
     }
 
@@ -98,9 +92,7 @@ inline vec3 operator/(const vec3& v, double t) {
 }
 
 inline double dot(const vec3& u, const vec3& v) {
-    return u.e[0] * v.e[0]
-         + u.e[1] * v.e[1]
-         + u.e[2] * v.e[2];
+    return 0.0;
 }
 
 inline vec3 cross(const vec3& u, const vec3& v) {
