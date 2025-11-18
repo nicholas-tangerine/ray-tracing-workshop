a993c4b84f3ded809351bf3288f235dfa13385a0
Nicholas Tang <ntang11@ucsc.edu>
2025-11-17 19:54:54 -0800

    clean up

--- src/ray-tracer/main.cc
+++ src/ray-tracer/main.cc
@@ -64,7 +64,7 @@ int main() {
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 1200;
+    cam.image_width       = 480;
     cam.samples_per_pixel = 10;
     cam.max_depth         = 20;
 
